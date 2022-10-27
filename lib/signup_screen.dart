c0eeb0a4b32e587937ad9fd6a0091f6aabcbc2a0
mariammagedd <112873455+mariammagedd@users.noreply.github.com>
2022-10-27 22:44:13 +0200

    Add files via upload

---
+++ lib/signup_screen.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
+
+class SignUpScreen extends StatefulWidget {
+  const SignUpScreen({super.key});
+
+  @override
+  State<SignUpScreen> createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends State<SignUpScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.black,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Form(
+              child: Column(
+                children: [
+                  TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      labelText: 'Email Address',
+                      hintText: 'Email Address',
+                      prefixIcon: Icon(Icons.email),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                            width: 3, color: Color.fromARGB(255, 212, 212, 80)),
+                      ),
+                      filled: true,
+                      fillColor: Color.fromARGB(255, 195, 186, 186),
+                    ),
+                    onChanged: (String value) {},
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  TextFormField(
+                    keyboardType: TextInputType.phone,
+                    decoration: InputDecoration(
+                      labelText: 'Phone Number',
+                      hintText: 'Phone Number',
+                      prefixIcon: Icon(Icons.phone),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                            width: 3, color: Color.fromARGB(255, 212, 212, 80)),
+                      ),
+                      filled: true,
+                      fillColor: Color.fromARGB(255, 195, 186, 186),
+                    ),
+                    onChanged: (String value) {},
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  TextFormField(
+                    keyboardType: TextInputType.name,
+                    decoration: InputDecoration(
+                      labelText: 'Username',
+                      hintText: 'Username',
+                      prefixIcon: Icon(Icons.person),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                            width: 3, color: Color.fromARGB(255, 212, 212, 80)),
+                      ),
+                      filled: true,
+                      fillColor: Color.fromARGB(255, 195, 186, 186),
+                    ),
+                    onChanged: (String value) {},
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  TextFormField(
+                    keyboardType: TextInputType.visiblePassword,
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      hintText: 'Password',
+                      prefixIcon: Icon(Icons.password),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                            width: 3, color: Color.fromARGB(255, 212, 212, 80)),
+                      ),
+                      filled: true,
+                      fillColor: Color.fromARGB(255, 195, 186, 186),
+                    ),
+                    onChanged: (String value) {},
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      //<-- SEE HERE
+                      backgroundColor: Color.fromARGB(255, 212, 212, 80),
+                      side: BorderSide(
+                        width: 3.0,
+                        color: Color.fromARGB(255, 212, 212, 80),
+                      ),
+                    ),
+                    child: Text('Submit'),
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    style: TextButton.styleFrom(
+                      //<-- SEE HERE
+                      side: BorderSide(width: 3.0),
+                    ),
+                    child: Text('Signup as an Admin'),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ));
+  }
+}
