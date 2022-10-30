67a3f40905515a23a0caa54c9c875e0088687d6b
nouka01 <96498521+nouka01@users.noreply.github.com>
2022-10-30 04:00:02 +0200

    Add files via upload

--- lib/screens/business_provider_packages_screen.dart
+++ lib/screens/business_provider_packages_screen.dart
@@ -21,9 +21,9 @@ class _PackagesScreenState extends State<PackagesScreen> {
                 style: TextStyle(fontSize: 34, color: Colors.white)),
             Row(mainAxisAlignment: MainAxisAlignment.center),
             SizedBox(height: 30),
-            buildPackageComponent("Package 1", "100 EGP          per month"),
-            buildPackageComponent("Package 2", "250 EGP          per 3 months"),
-            buildPackageComponent("Package 3", "999 EGP          per year")
+            buildPackageComponent("Package 1", "100 EGP per month"),
+            buildPackageComponent("Package 2", "250 EGP per 3months"),
+            buildPackageComponent("Package 3", "999 EGP per year")
 
 
           ],
@@ -47,11 +47,12 @@ Widget buildPackageComponent(String packageName,String pricePerDate){
           height: 50,
           padding: EdgeInsets.only(left:65),
         ),
+
         SizedBox(height: 0),
-        Container(child: Text(pricePerDate, style: TextStyle(fontSize: 30, color: Colors.white)),
-          height: 120,
+        Container(child: Text(pricePerDate, style: TextStyle(fontSize: 22, color: Colors.white)),
+          height: 80,
           width:300,
-          padding: EdgeInsets.only(left:75, top: 10),
+          padding: EdgeInsets.only(left:55, top: 10),
           decoration: BoxDecoration(
               color: Colors.black,
               border: Border.all(
@@ -61,6 +62,14 @@ Widget buildPackageComponent(String packageName,String pricePerDate){
 
 
         ),
+              ElevatedButton(
+
+                onPressed: () { },
+                child: Text('Subscribe'),
+                style: ElevatedButton.styleFrom(
+                    primary: Colors.green
+                ),
+              ),
 
 
         SizedBox(height: 30)
