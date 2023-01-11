eaf678246e86f1909a248f2e237a2a23e867a00d
OmarSaad0-0 <89510329+OmarSaad0-0@users.noreply.github.com>
2023-01-11 18:43:29 +0200

    Add files via upload

---
+++ lib/widgets/BussinessOwner_Navigation_Drawer.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:sports_registration/Pages/Add_New_Field.dart';
+import 'package:sports_registration/Pages/my_Fields.dart';
+import 'package:sports_registration/Pages/BussinessOwner_Settings.dart';
+import 'package:sports_registration/widgets/drawer_item.dart';
+
+class NavigationDraw extends StatelessWidget {
+  const NavigationDraw({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Drawer(
+      child: Material(
+        color: Color(0xFF303030),
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(24.0, 80, 24, 0),
+          child: Column(children: [
+            headerWidget(),
+            SizedBox(
+              height: 20,
+            ),
+            DrawerItem(
+              name: 'My Fields',
+              onPressed: () => onItemPressed(context, index: 0),
+            ),
+            DrawerItem(
+              name: 'Add Field',
+              onPressed: () => onItemPressed(context, index: 1),
+            ),
+            DrawerItem(
+              name: 'Account Settings',
+              onPressed: () => onItemPressed(context, index: 2),
+            ),
+            TextButton(
+              style: ButtonStyle(
+                foregroundColor: MaterialStateProperty.all<Color>(
+                    Color.fromARGB(255, 232, 25, 25)),
+              ),
+              onPressed: () {},
+              child: Text('SignOut'),
+            )
+          ]),
+        ),
+      ),
+    );
+  }
+
+  void onItemPressed(BuildContext context, {required int index}) {
+    Navigator.pop(context);
+    switch (index) {
+      case 0:
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => const MyFields()));
+        break;
+      case 1:
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => const NewField()));
+        break;
+      case 2:
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => const Settings()));
+        break;
+    }
+  }
+
+  Widget headerWidget() {
+    const url = 'C:/Users/Dell/Downloads/app.png';
+    return Row(
+      children: [
+        const SizedBox(
+          width: 20,
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const [
+            Text('Bussiness Owner Name',
+                style: TextStyle(fontSize: 14, color: Colors.white)),
+            SizedBox(
+              height: 10,
+            ),
+            Text('Owner@email.com',
+                style: TextStyle(fontSize: 14, color: Colors.white))
+          ],
+        )
+      ],
+    );
+  }
+}
