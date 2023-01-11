96ebc7a9444b11b66f0c2b993bf0f056af1672b1
SeifMKandil <100947401+SeifMKandil@users.noreply.github.com>
2023-01-11 03:15:26 +0200

    Add files via upload

---
+++ lib/view-models/auth_view_model.dart
@@ -0,0 +1,80 @@
+import 'package:e7gzly/models/user_model.dart';
+import 'package:e7gzly/views/home_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+import 'package:get/get.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+import '../services/firestore_user.dart';
+
+class AuthViewModel extends GetxController {
+  GoogleSignIn _gooleSignin = GoogleSignIn(scopes: ['email']);
+  FirebaseAuth _auth = FirebaseAuth.instance;
+
+  late String email, password, name;
+  late int number;
+
+  Rxn<User> _user = Rxn<User>();
+
+  String? get user => _user.value!.email;
+  @override
+  void onInit() {
+    super.onInit();
+    _user.bindStream(_auth.authStateChanges());
+  }
+
+  void googleSignInMethod() async {
+    final GoogleSignInAccount? googleuser = await _gooleSignin.signIn();
+    print("ahmed");
+    GoogleSignInAuthentication? googleSignInAuthentication =
+        await googleuser?.authentication;
+
+    final AuthCredential credential = GoogleAuthProvider.credential(
+      idToken: googleSignInAuthentication!.idToken,
+      accessToken: googleSignInAuthentication.accessToken,
+    );
+    await _auth.signInWithCredential(credential).then((user) async {
+      await FireStoreUser().addUserToFireStore(UserModel(
+        userId: user.user?.uid,
+        email: user.user?.email,
+        name: user.user?.displayName,
+        pic: '',
+        number: null,
+      ));
+    });
+  }
+
+  void signInwithEmailAndPassword() async {
+    try {
+      await _auth
+          .signInWithEmailAndPassword(email: email, password: password)
+          .then((value) {
+        print(value);
+      });
+    } on FirebaseAuthException catch (e) {
+      Get.snackbar("Error while creating account", e.message.toString(),
+          snackPosition: SnackPosition.BOTTOM);
+    }
+  }
+
+  void creatAccountwithEmailAndPassword() async {
+    try {
+      await _auth
+          .createUserWithEmailAndPassword(email: email, password: password)
+          .then((user) async {
+        await FireStoreUser().addUserToFireStore(UserModel(
+          userId: user.user?.uid,
+          email: user.user?.email,
+          name: user.user?.displayName,
+          pic: '',
+          number: number,
+        ));
+      });
+
+      Get.offAll(HomePage());
+    } on FirebaseAuthException catch (e) {
+      Get.snackbar("Error while creating account", e.message.toString(),
+          snackPosition: SnackPosition.BOTTOM);
+    }
+  }
+}
---
+++ lib/views/calendar.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+import '../widgets/slot_button.dart';
+
+class Calendar extends StatefulWidget {
+  const Calendar({super.key});
+
+  @override
+  State<Calendar> createState() => _CalendarState();
+}
+
+class _CalendarState extends State<Calendar> {
+  DateTime today = DateTime.now();
+  void _onDaySelected(DateTime day, DateTime focusedDay) {
+    setState(() {
+      today = day;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: content(),
+    );
+  }
+
+  Widget content() {
+    return Column(
+      children: [
+        SizedBox(
+          height: 40,
+        ),
+        Text(today.toString().split(" ")[0]),
+        Container(
+            child: TableCalendar(
+          rowHeight: 60,
+          headerStyle: const HeaderStyle(
+              formatButtonVisible: false, titleCentered: true),
+          availableGestures: AvailableGestures.all,
+          selectedDayPredicate: ((day) => isSameDay(day, today)),
+          focusedDay: today,
+          firstDay: DateTime.utc(2010, 10, 16),
+          lastDay: DateTime.utc(2030, 3, 14),
+          onDaySelected: _onDaySelected,
+          calendarStyle: const CalendarStyle(
+            todayDecoration: const BoxDecoration(
+              color: Color.fromARGB(255, 162, 162, 162),
+              shape: BoxShape.circle,
+            ),
+            selectedDecoration: const BoxDecoration(
+              color: Color(0xFF212121),
+              shape: BoxShape.circle,
+            ),
+            isTodayHighlighted: true,
+            //selectedDecoration: BoxDecoration(),
+          ),
+        )),
+        Slot_Button(),
+      ],
+    );
+  }
+}
