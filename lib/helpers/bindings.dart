1018a7802f73d6e2583649e53b04a90d9fe54dbf
SeifMKandil <100947401+SeifMKandil@users.noreply.github.com>
2023-01-13 06:37:02 +0200

    Add files via upload

--- lib/helpers/bindings.dart
+++ lib/helpers/bindings.dart
@@ -1,9 +1,11 @@
 import 'package:e7gzly/view-models/auth_view_model.dart';
+import 'package:e7gzly/view-models/home_view_model.dart';
 import 'package:get/get.dart';
 
 class Binding extends Bindings {
   @override
   void dependencies() {
     Get.lazyPut(() => AuthViewModel());
+    Get.lazyPut(() => HomeViewModel());
   }
 }
