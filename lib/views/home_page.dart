8b386857b741c8eecbe17c70428f698e9173fd3d
SeifMKandil <100947401+SeifMKandil@users.noreply.github.com>
2023-01-13 06:34:27 +0200

    Add files via upload
    Home_Screen is now dynamic

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,15 +1,15 @@
+import 'package:e7gzly/view-models/home_view_model.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 import '../widgets/NavBarr.dart';
 import '../widgets/card_text.dart';
-import '../widgets/categories_scroll.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return GetBuilder<HomeViewModel>(
+      builder: (controller) => Scaffold(
         drawer: const NavBarr(),
         backgroundColor: const Color(0xFF212121),
         appBar: AppBar(
@@ -19,68 +19,78 @@ class HomePage extends StatelessWidget {
                   fontWeight: FontWeight.bold,
                   color: Colors.white.withOpacity(0.8))),
         ),
-        body: SafeArea(
-            child: SingleChildScrollView(
-                child: Column(children: <Widget>[
-          const SizedBox(
-            height: 30,
+        body: SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.only(top: 20),
+            child: Column(children: [
+              _listViewCategory(),
+///////////////////////////////////////////////////////////////////////////////////
+              //SearchBar(),
+              SizedBox(
+                height: 30,
+              ),
+              Container(
+                height: MediaQuery.of(context).size.height * .67,
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: GetBuilder<HomeViewModel>(
+                    builder: (controller) => ListView.separated(
+                      shrinkWrap: true,
+                      itemCount: controller.feildDetailsModel.length,
+                      scrollDirection: Axis.vertical,
+                      itemBuilder: (context, index) {
+                        return CardText(
+                            text: controller.feildDetailsModel[index].location,
+                            text1: controller.feildDetailsModel[index].name,
+                            price: controller.feildDetailsModel[index].price,
+                            image: controller.feildDetailsModel[index].image,
+                            text0: "s");
+                      },
+                      separatorBuilder: (context, index) => SizedBox(
+                        height: 20,
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ]),
           ),
-          SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                children: [
-                  buildCard(
-                      image: Image.asset("assets/images/foot.jpg",
-                          fit: BoxFit.fill),
-                      category: "Football"),
-                  buildCard(
-                      image: Image.asset("assets/images/voley.jpg",
-                          fit: BoxFit.fill),
-                      category: "Volleyball"),
-                  buildCard(
-                      image: Image.asset("assets/images/basket.jpg",
-                          fit: BoxFit.fill),
-                      category: "Basketball"),
-                  buildCard(
-                      image: Image.asset("assets/images/tennis.jpg",
-                          fit: BoxFit.fill),
-                      category: "Tennis"),
-                  buildCard(
-                      image: Image.asset("assets/images/baskett.jpg",
-                          fit: BoxFit.fill),
-                      category: "Basketball"),
-                ],
-              )),
-          SizedBox(
-            height: 20,
+        ),
+      ),
+    );
+  }
+
+// The List-View Widget of categories
+  _listViewCategory() {
+    return GetBuilder<HomeViewModel>(
+      builder: (controller) => Container(
+        height: 100,
+        child: ListView.separated(
+          itemCount: controller.categoryModel.length,
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (context, index) {
+            return Column(
+              children: [
+                Container(
+                  height: 60,
+                  width: 120,
+                  child: Image.network(controller.categoryModel[index].image),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  controller.categoryModel[index].name,
+                  style: TextStyle(color: Color.fromARGB(255, 248, 247, 247)),
+                )
+              ],
+            );
+          },
+          separatorBuilder: (context, index) => SizedBox(
+            width: 5,
           ),
-          //SearchBar(),
-          SizedBox(
-            height: 30,
-          ),
-          CardText(
-            text1: "El Galaa club",
-            text: "Masr El Gedida, Salah Salem",
-            text0: "Football",
-            price: " Price Per Hour: 200 LE",
-            image: Image.asset("assets/images/1.jpg", fit: BoxFit.fill),
-          ),
-          const SizedBox(height: 30),
-          CardText(
-            text1: "El Lycee School",
-            text: "Masr El Gdida, Triumph",
-            text0: "Volleyball",
-            price: " Price Per Hour: 150 LE",
-            image: Image.asset("assets/images/volloey.jpg", fit: BoxFit.fill),
-          ),
-          const SizedBox(height: 30),
-          CardText(
-            text1: "El Wafaa Wel Amal",
-            text: "Nasr City, Mostafa El Nahas",
-            text0: "Padel",
-            price: " Price Per Hour: 250 LE",
-            image: Image.asset("assets/images/padel(1).jpg", fit: BoxFit.fill),
-          )
-        ]))));
+        ),
+      ),
+    );
   }
 }
