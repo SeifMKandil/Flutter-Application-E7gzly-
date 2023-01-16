2d1d19e72735798003a2f6a355eb0806f59e483f
SeifMKandil <100947401+SeifMKandil@users.noreply.github.com>
2023-01-16 04:49:59 +0200

    Add files via upload

--- lib/view-models/auth_view_model.dart
+++ lib/view-models/auth_view_model.dart
@@ -1,4 +1,5 @@
 import 'package:e7gzly/models/user_model.dart';
+import 'package:e7gzly/views/control_view.dart';
 import 'package:e7gzly/views/home_page.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/animation.dart';
@@ -6,6 +7,7 @@ import 'package:flutter/animation.dart';
 import 'package:get/get.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
+import '../helpers/local_storage.dart';
 import '../services/firestore_user.dart';
 
 class AuthViewModel extends GetxController {
@@ -14,10 +16,11 @@ class AuthViewModel extends GetxController {
 
   late String email, password, name;
   late int number;
+  final LocalStorageData localStorageData = Get.find();
 
   Rxn<User> _user = Rxn<User>();
 
-  String? get user => _user.value?.email;
+  String? get user => _user.value!.email;
   @override
   void onInit() {
     super.onInit();
@@ -26,6 +29,7 @@ class AuthViewModel extends GetxController {
 
   void googleSignInMethod() async {
     final GoogleSignInAccount? googleuser = await _gooleSignin.signIn();
+
     print("ahmed");
     GoogleSignInAuthentication? googleSignInAuthentication =
         await googleuser?.authentication;
@@ -42,16 +46,36 @@ class AuthViewModel extends GetxController {
         pic: '',
         number: null,
       ));
+      setUser(UserModel(
+        userId: user.user?.uid,
+        email: user.user?.email,
+        name: user.user?.displayName,
+        pic: '',
+        number: null,
+      ));
+      Get.offAll(ControlView());
     });
   }
 
   void signInwithEmailAndPassword() async {
     try {
-      await _auth.signInWithEmailAndPassword(email: email, password: password);
-      Get.offAll(HomePage());
-    } on FirebaseAuthException catch (e) {
-      Get.snackbar("Error while creating account", e.message.toString(),
-          colorText: Color(15), snackPosition: SnackPosition.BOTTOM);
+      await _auth
+          .signInWithEmailAndPassword(
+              email: email.trim(), password: password.trim())
+          .then((value) async {
+        try {
+          await FireStoreUser().getCurrentUser(value.user!.uid).then((value) =>
+              {
+                setUser(
+                    UserModel.fromJson(value.data() as Map<dynamic, dynamic>))
+              });
+        } catch (e) {
+          print(e);
+        }
+      });
+      Get.offAll(ControlView());
+    } catch (e) {
+      print(password);
     }
   }
 
@@ -63,16 +87,33 @@ class AuthViewModel extends GetxController {
         await FireStoreUser().addUserToFireStore(UserModel(
           userId: user.user?.uid,
           email: user.user?.email,
-          name: user.user?.displayName,
+          name: name,
           pic: '',
           number: number,
         ));
+        setUser(UserModel(
+          userId: user.user?.uid,
+          email: user.user?.email,
+          name: user.user?.displayName,
+          pic: '',
+          number: null,
+        ));
       });
 
-      Get.offAll(HomePage());
+      Get.offAll(ControlView());
     } on FirebaseAuthException catch (e) {
       Get.snackbar("Error while creating account", e.message.toString(),
           snackPosition: SnackPosition.BOTTOM);
     }
+  }
+
+  Future<void> logOut() async {
+    GoogleSignIn().signOut();
+    FirebaseAuth.instance.signOut();
+    localStorageData.deleteUser();
+  }
+
+  void setUser(UserModel userModel) async {
+    await localStorageData.setUser(userModel);
   }
 }
