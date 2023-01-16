36fe9a1ba71041f4b1aeb44336f98b336d8237f4
SeifMKandil <100947401+SeifMKandil@users.noreply.github.com>
2023-01-16 04:50:51 +0200

    Add files via upload
    Navigate to Logout , Navigate to Profile Screen

--- lib/widgets/NavBarr.dart
+++ lib/widgets/NavBarr.dart
@@ -1,8 +1,12 @@
+import 'package:e7gzly/view-models/auth_view_model.dart';
 import 'package:e7gzly/views/login_view.dart';
+import 'package:e7gzly/views/my_profile_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
+
+import '../view-models/profile_view_model.dart';
 
 class NavBarr extends StatelessWidget {
   // FirebaseAuth _auth = FirebaseAuth.instance;
@@ -10,46 +14,47 @@ class NavBarr extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    FirebaseAuth _auth = FirebaseAuth.instance;
-
-    return Drawer(
-      child: ListView(
-        children: [
-          Image.asset("images/logo.png"),
-          ListTile(
-            leading: const Icon(Icons.account_circle),
-            title: Text('My profile'),
-            onTap: () => null,
-          ),
-          ListTile(
-            leading: Icon(Icons.notification_important),
-            title: Text('Notifications'),
-            onTap: () => null,
-          ),
-          ListTile(
-            leading: Icon(Icons.share),
-            title: Text('Share'),
-            onTap: () => null,
-          ),
-          ListTile(
-            leading: Icon(Icons.favorite),
-            title: Text('Favorites'),
-            onTap: () => null,
-          ),
-          ListTile(
-            leading: Icon(Icons.settings),
-            title: Text('Settings'),
-            onTap: () => null,
-          ),
-          ListTile(
-            leading: Icon(Icons.exit_to_app),
-            title: Text('Log out'),
-            onTap: () {
-              _auth.signOut();
-              Get.offAll(LoginView());
-            },
-          ),
-        ],
+    return GetBuilder<AuthViewModel>(
+      init: AuthViewModel(),
+      builder: (controller) => Drawer(
+        child: ListView(
+          children: [
+            Image.asset("images/logo.png"),
+            ListTile(
+              leading: const Icon(Icons.account_circle),
+              title: Text('My profile'),
+              onTap: () => Get.to(EditProfilePage()),
+            ),
+            ListTile(
+              leading: Icon(Icons.notification_important),
+              title: Text('Notifications'),
+              onTap: () => null,
+            ),
+            ListTile(
+              leading: Icon(Icons.share),
+              title: Text('Share'),
+              onTap: () => null,
+            ),
+            ListTile(
+              leading: Icon(Icons.favorite),
+              title: Text('Favorites'),
+              onTap: () => null,
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Settings'),
+              onTap: () => null,
+            ),
+            ListTile(
+              leading: Icon(Icons.exit_to_app),
+              title: Text('Log out'),
+              onTap: () {
+                controller.logOut();
+                Get.offAll(LoginView());
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
