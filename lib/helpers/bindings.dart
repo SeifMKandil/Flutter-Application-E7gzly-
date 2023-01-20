0aba7ba198c3516174fc88364c56c848148ca34e
Mariam1904317 <96684192+Mariam1904317@users.noreply.github.com>
2023-01-20 23:12:24 +0200

    Update bindings.dart

--- lib/helpers/bindings.dart
+++ lib/helpers/bindings.dart
@@ -3,6 +3,8 @@ import 'package:e7gzly/view-models/auth_view_model.dart';
 import 'package:e7gzly/view-models/home_view_model.dart';
 import 'package:e7gzly/views/control_view.dart';
 import 'package:get/get.dart';
+import '../view-models/reservations_view_model.dart';
+import '../view-models/field_view_model.dart';
 
 class Binding extends Bindings {
   @override
