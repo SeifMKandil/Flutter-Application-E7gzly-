0c90f414041476bed9ce37a42ef8d5289ba2e6e4
nouka01 <96498521+nouka01@users.noreply.github.com>
2023-01-20 18:32:03 +0200

    Add files via upload
    Added initializations needed for push notifications

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,38 @@ import 'package:get/get.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'helpers/bindings.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+
+const AndroidNotificationChannel channel = AndroidNotificationChannel(
+  'high_importance_channel',
+  'High Importance Notification',
+importance: Importance.high,
+  playSound: true
+);
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+
+}
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
   Stripe.publishableKey = "pk_test_51M30wnG5FKQlBCQ1QMLmIfNFuIgd8jcZndZrts5d66J7Uned0HRxN90lhlM7582cMfXKl84WNJGLZolVXKUgKacQ00HNMZ1cEc";
   await Firebase.initializeApp();
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+  await flutterLocalNotificationsPlugin.resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>()?.createNotificationChannel(channel);
+
+  await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+    alert: true,
+    badge: true,
+    sound: true
+  );
+
   runApp(MyApp());
 }
 
