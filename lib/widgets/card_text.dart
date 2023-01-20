70914ee1f408d8f92c88d489bee3bfe6ab40c83b
OmarSaad0-0 <89510329+OmarSaad0-0@users.noreply.github.com>
2023-01-20 12:58:47 +0200

    Add files via upload

--- lib/widgets/card_text.dart
+++ lib/widgets/card_text.dart
@@ -25,135 +25,136 @@ class CardText extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetBuilder<HomeViewModel>(
         builder: (controller) => Center(
-      child: InkWell(
-        onTap: () {
+              child: InkWell(
+                onTap: () {
+                  FeildDetailsModel fieldDetails = FeildDetailsModel(
+                      name: text, price: price, image: image, location: text1);
+                  controller.setFieldDetails(fieldDetails);
 
-           FeildDetailsModel fieldDetails = FeildDetailsModel(name: text, price: price, image: image, location: text1);
-            controller.setFieldDetails(fieldDetails);
+                  Get.to(Calendar(), arguments: {
+                    'name': text,
+                    'price': price,
+                    'location': text1,
+                  }); //page fadya batest beha
+                },
+                child: Container(
+                  width: 300,
+                  height: 320,
+                  decoration: BoxDecoration(
+                      color: const Color(0xFF212121),
+                      border: Border.all(color: Color.fromARGB(255, 0, 0, 0)),
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Color.fromARGB(255, 0, 0, 0),
+                          spreadRadius: 2,
+                          blurRadius: 5,
+                        )
+                      ]),
+                  //sooooooooooooooooooooooooooooooooooooooooooooora
 
-
-          Get.to(Calendar()); //page fadya batest beha
-        
-        },
-        child: Container(
-          width: 300,
-          height: 320,
-          decoration: BoxDecoration(
-              color: const Color(0xFF212121),
-              border: Border.all(color: Color.fromARGB(255, 0, 0, 0)),
-              boxShadow: const [
-                BoxShadow(
-                  color: Color.fromARGB(255, 0, 0, 0),
-                  spreadRadius: 2,
-                  blurRadius: 5,
-                )
-              ]),
-          //sooooooooooooooooooooooooooooooooooooooooooooora
-
-          child: Column(
-            children: <Widget>[
-              const SizedBox(
-                height: 15,
-              ),
-              Container(
-                width: 270,
-                height: 150,
-
-                child: Image.network(image, fit: BoxFit.fill),
-
-              ),
-              //teeeeeeeeeeeeeeeeexxxxxxxxxxxxxxtttttttt
-              const SizedBox(
-                height: 20,
-              ),
-
-              Container(
-                  child: Column(children: <Widget>[
-                Row(children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 13),
-                    child: Text(
-                      text1,
-                      style: const TextStyle(
-                          fontSize: 23,
-                          fontWeight: FontWeight.bold,
-                          color: Color(0xFFF5F5F5)),
-                    ),
-                  )
-                ]),
-                const SizedBox(
-                  height: 5,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 9),
-                  child: Row(children: <Widget>[
-                    const Icon(
-                      Icons.place_outlined,
-                      color: Colors.redAccent,
-                      size: 25,
-                    ),
-                    Text(
-                      text,
-                      style: const TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                          color: Color(0xFFF5F5F5)),
-                    )
-                  ]),
-                ),
-                const SizedBox(
-                  height: 5,
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 14),
-                  child: Row(children: [
-                    const Text(
-                      "Rating:",
-                      style: TextStyle(fontSize: 15, color: Color(0xFFF5F5F5)),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(2.0),
-                      child: RatingBar.builder(
-                        initialRating: 4,
-                        minRating: 1,
-                        direction: Axis.horizontal,
-                        allowHalfRating: true,
-                        itemCount: 5,
-                        itemSize: 20,
-                        itemPadding:
-                            const EdgeInsets.symmetric(horizontal: 4.0),
-                        itemBuilder: (context, _) => const Icon(
-                          Icons.star,
-                          color: Colors.amber,
-                        ),
-                        onRatingUpdate: (rating) {
-                          print(rating);
-                        },
+                  child: Column(
+                    children: <Widget>[
+                      const SizedBox(
+                        height: 15,
                       ),
-                    ),
-                  ]),
-                ),
-                const SizedBox(
-                  height: 5,
-                ),
-                Align(
-                    alignment: Alignment.bottomLeft,
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 16),
-                      child: Text(
-                        price,
-                        style: const TextStyle(
-                          fontSize: 15,
-                          color: Color(0xFFF5F5F5),
-                        ),
+                      Container(
+                        width: 270,
+                        height: 150,
+                        child: Image.network(image, fit: BoxFit.fill),
                       ),
-                    )),
-              ]))
-            ],
-          ),
-        ),
-      ),
-        ));
+                      //teeeeeeeeeeeeeeeeexxxxxxxxxxxxxxtttttttt
+                      const SizedBox(
+                        height: 20,
+                      ),
 
+                      Container(
+                          child: Column(children: <Widget>[
+                        Row(children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 13),
+                            child: Text(
+                              text1,
+                              style: const TextStyle(
+                                  fontSize: 23,
+                                  fontWeight: FontWeight.bold,
+                                  color: Color(0xFFF5F5F5)),
+                            ),
+                          )
+                        ]),
+                        const SizedBox(
+                          height: 5,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 9),
+                          child: Row(children: <Widget>[
+                            const Icon(
+                              Icons.place_outlined,
+                              color: Colors.redAccent,
+                              size: 25,
+                            ),
+                            Text(
+                              text,
+                              style: const TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                  color: Color(0xFFF5F5F5)),
+                            )
+                          ]),
+                        ),
+                        const SizedBox(
+                          height: 5,
+                        ),
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 14),
+                          child: Row(children: [
+                            const Text(
+                              "Rating:",
+                              style: TextStyle(
+                                  fontSize: 15, color: Color(0xFFF5F5F5)),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(2.0),
+                              child: RatingBar.builder(
+                                initialRating: 4,
+                                minRating: 1,
+                                direction: Axis.horizontal,
+                                allowHalfRating: true,
+                                itemCount: 5,
+                                itemSize: 20,
+                                itemPadding:
+                                    const EdgeInsets.symmetric(horizontal: 4.0),
+                                itemBuilder: (context, _) => const Icon(
+                                  Icons.star,
+                                  color: Colors.amber,
+                                ),
+                                onRatingUpdate: (rating) {
+                                  print(rating);
+                                },
+                              ),
+                            ),
+                          ]),
+                        ),
+                        const SizedBox(
+                          height: 5,
+                        ),
+                        Align(
+                            alignment: Alignment.bottomLeft,
+                            child: Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 16),
+                              child: Text(
+                                price,
+                                style: const TextStyle(
+                                  fontSize: 15,
+                                  color: Color(0xFFF5F5F5),
+                                ),
+                              ),
+                            )),
+                      ]))
+                    ],
+                  ),
+                ),
+              ),
+            ));
   }
 }
