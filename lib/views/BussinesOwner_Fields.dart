fc08f3a1f5d82c0e4c8cfc7ab517bca86143ebf0
Mariam1904317 <96684192+Mariam1904317@users.noreply.github.com>
2023-01-20 22:12:27 +0200

    Update BussinesOwner_Fields.dart

--- lib/views/BussinesOwner_Fields.dart
+++ lib/views/BussinesOwner_Fields.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 import '../widgets/BussinessOwner_Display_Fields_Cards.dart';
+import '../view-models/field_view_model.dart';
+import 'package:e7gzly/view-models/home_view_model.dart';
+import 'package:get/get.dart';
+
+import '../widgets/NavBarr.dart';
+import '../widgets/card_text.dart';
 
 class MyFields extends StatelessWidget {
   const MyFields({Key? key}) : super(key: key);
@@ -7,46 +13,40 @@ class MyFields extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Scaffold(
-      backgroundColor: Color(0xFF212121),
-      appBar: AppBar(
-        title: const Text('Fields'),
-        centerTitle: true,
-        backgroundColor: Color(0xFF303030),
-      ),
-      body: SingleChildScrollView(
-        child: Center(
-          child: Container(
-            child: Column(children: [
-              SizedBox(
-                height: 20,
+    return GetBuilder<FieldViewModel>(
+      builder: (controller) => Scaffold(
+        backgroundColor: Color(0xFF212121),
+        appBar: AppBar(
+          title: const Text('Fields'),
+          centerTitle: true,
+          backgroundColor: Color(0xFF303030),
+        ),
+        body: SingleChildScrollView(
+          child: Center(
+              child: Container(
+            height: MediaQuery.of(context).size.height,
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: GetBuilder<FieldViewModel>(
+                builder: (controller) => ListView.separated(
+                  shrinkWrap: true,
+                  itemCount: controller.feildDetailsModel.length,
+                  scrollDirection: Axis.vertical,
+                  itemBuilder: (context, index) {
+                    return FieldsCardText(
+                      place: controller.feildDetailsModel[index].location,
+                      Field: controller.feildDetailsModel[index].name,
+                      price: controller.feildDetailsModel[index].price,
+                      
+                    );
+                  },
+                  separatorBuilder: (context, index) => SizedBox(
+                    height: 20,
+                  ),
+                ),
               ),
-              FieldsCardText(
-                Field: '1',
-                price: '200',
-                place: 'nasr city',
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              FieldsCardText(
-                Field: '5',
-                place: 'new cairo',
-                price: '300',
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              FieldsCardText(
-                Field: '6',
-                place: 'shubra',
-                price: '100',
-              ),
-              SizedBox(
-                height: 20,
-              ),
-            ]),
-          ),
+            ),
+          )),
         ),
       ),
     );
