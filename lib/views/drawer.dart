e68e85e093167ce390370cfb0280a2281c13acbf
SeifMKandil <100947401+SeifMKandil@users.noreply.github.com>
2023-01-21 08:18:38 +0200

    Add files via upload

--- lib/views/drawer.dart
+++ lib/views/drawer.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 import '../widgets/theme_changer.dart';
 
-
 class drawer extends StatelessWidget {
   const drawer({super.key});
 
@@ -11,63 +10,56 @@ class drawer extends StatelessWidget {
     return Scaffold(
       drawer: Drawer(
         child: ListView(
-          children: <Widget> [
+          children: <Widget>[
             DrawerHeader(
-decoration: BoxDecoration(
-  color: Colors.black
-),
-         padding: EdgeInsets.all(10),
-          child: Column(children:<Widget> [
-            Container(
-              width: 70,
-              height: 70,
-              child: CircleAvatar(
-                backgroundImage: AssetImage('assets/images/1.jpg'),
-              )
-
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Text(
-              'Aly Khaled',
-              style: TextStyle(fontSize: 18, color: Colors.white),
-            ),
-            Text(
-              'lolo@gmail.com',
-              style: TextStyle(fontSize: 14, color: Colors.white),
-
-            )
-
-          ]),
-            
+              decoration: BoxDecoration(color: Colors.black),
+              padding: EdgeInsets.all(10),
+              child: Column(children: <Widget>[
+                Container(
+                    width: 70,
+                    height: 70,
+                    child: CircleAvatar(
+                      backgroundImage: AssetImage('assets/images/1.jpg'),
+                    )),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  'Aly Khaled',
+                  style: TextStyle(fontSize: 18, color: Colors.white),
+                ),
+                Text(
+                  'lolo@gmail.com',
+                  style: TextStyle(fontSize: 14, color: Colors.white),
+                )
+              ]),
             ),
             ListTile(
               leading: Icon(Icons.home),
               title: Text('Profile'),
-              onTap: (){
+              onTap: () {
                 print('Profile clicked');
               },
             ),
             ListTile(
               leading: Icon(Icons.home),
               title: Text('Previous'),
-              onTap: (){
+              onTap: () {
                 print('Previous clicked');
               },
             ),
             ListTile(
               leading: Icon(Icons.home),
               title: Text('Language'),
-              onTap: (){
+              onTap: () {
                 print('Language clicked');
               },
             ),
             ListTile(
               leading: Icon(Icons.home),
               title: Text('Dark/Light Theme'),
-              onTap: (){
-                 ThemeBuilder.of(context)?.changeTheme();
+              onTap: () {
+                // ThemeBuilder.of(context)?.changeTheme();
               },
             ),
           ],
